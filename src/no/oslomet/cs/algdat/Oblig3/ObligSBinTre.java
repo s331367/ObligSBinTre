45cde6334b89bd74abd1f07b563901e02e58b9c0
Rizwan Mahmood <s331409@oslomet.no>
2019-11-05 14:26:23 +0100

    Gjort Opg 5 og 7, test passed

--- src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
+++ src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
@@ -98,62 +98,69 @@ public class ObligSBinTre<T> implements Beholder<T>
   @Override
   public boolean fjern(T verdi)
   {
-    if(verdi == null) return false;
+    if (verdi == null) return false;  // treet har ingen nullverdier
 
-    Node<T> p = rot;
+    Node<T> p = rot, q = null;   // q skal være forelder til p
 
-    while(p!=null){
-      int cmp = comp.compare(verdi,p.verdi);
-
-      if(cmp < 0) p=p.venstre;
-      else if(cmp > 0) p=p.høyre;
-      else break;
+    while (p != null)            // leter etter verdi
+    {
+      int cmp = comp.compare(verdi,p.verdi);      // sammenligner
+      if (cmp < 0) { q = p; p = p.venstre; }      // går til venstre
+      else if (cmp > 0) { q = p; p = p.høyre; }   // går til høyre
+      else break;    // den søkte verdien ligger i p
     }
+    if (p == null) return false;   // finner ikke verdi
 
-    if (p==null) return false;
-
-    if (p.venstre==null || p.høyre==null) {
-
-      Node<T> b = (p.venstre!=null) ? p.venstre : p.høyre;
-
+    if (p.venstre == null || p.høyre == null)  // Tilfelle 1) og 2)
+    {
+      Node<T> b = p.venstre != null ? p.venstre : p.høyre;  // b for barn
       if (p == rot) {
-        rot =  b;
-        if(b!=null) b.forelder=null;
+        rot = b;
+        if(b!=null) b.forelder = null;
       }
-      else if (p==p.forelder.venstre) {
-        if(b!=null)b.forelder = p.forelder;
-        p.forelder.venstre = b;
-      } else {
-
-        if(b!=null)b.forelder = p.forelder;
-        p.forelder.høyre = b;
+      else if (p == q.venstre) {
+        q.venstre = b;
+        if(b!=null) b.forelder = q;
       }
-    }
-    else {
-
-      Node<T> r = p.høyre;
-      while (r.venstre != null) r = r.venstre;
-      p.verdi = r.verdi;
-
-      if(r.forelder!=p) {
-        Node<T> q = r.forelder;
-        q.venstre = r.høyre;
-        if(q.venstre!=null)q.venstre.forelder = q;
-      }
-      else{
-        p.høyre =  r.høyre;
-        if(p.høyre !=null) p.høyre.forelder = p;
-
+      else {
+        q.høyre = b;
+        if(b!=null)b.forelder=q;
       }
     }
+    else  // Tilfelle 3)
+    {
+      Node<T> s = p, r = p.høyre;   // finner neste i inorden
+      while (r.venstre != null)
+      {
+        s = r;    // s er forelder til r
+        r = r.venstre;
+      }
 
-    antall--;
+      p.verdi = r.verdi;   // kopierer verdien i r til p
+
+      if (s != p) {
+        s.venstre = r.høyre;
+        if(s.venstre!=null)s.venstre.forelder = s;
+      }
+      else {
+        s.høyre = r.høyre;
+        if(s.høyre != null)s.høyre.forelder = s;
+      }
+    }
+
+    antall--;   // det er nå én node mindre i treet
     return true;
   }
   
   public int fjernAlle(T verdi)
   {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+    int teller = 0;
+    boolean fjernet = true;
+    while (fjernet!=false){
+      if(fjern(verdi)) teller++;
+      else fjernet = false;
+    }
+    return teller;
   }
   
   @Override
@@ -187,7 +194,22 @@ public class ObligSBinTre<T> implements Beholder<T>
   @Override
   public void nullstill()
   {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+    if(!tom()) nullstill(rot);
+    rot = null;
+    antall = 0;
+    endringer++;
+  }
+  private void nullstill(Node<T> n){
+    if(n.venstre!=null){
+      nullstill(n.venstre);
+      n.venstre=null;
+    }
+    if(n.høyre!=null){
+      nullstill(n.høyre);
+      n.høyre=null;
+    }
+    n.forelder=null;
+    n.verdi=null;
   }
 
   private static <T> Node<T> nesteInorden(Node<T> p)
@@ -458,17 +480,8 @@ public class ObligSBinTre<T> implements Beholder<T>
     ObligSBinTre<Character> tre = new ObligSBinTre<>(Comparator.naturalOrder());
     char[] verdier = "IATBHJCRSOFELKGDMPQN".toCharArray();
     for (char c : verdier) tre.leggInn(c);
-     System.out.println(tre.høyreGren()+ " " + tre.lengstGren());
-    //Utskrift: [I, T, J, R, S] [I, A, B, H, C, F, E, D]
-    String[] s = tre.grener();
-    for (String gren : s) System.out.println(gren);
-// Utskrift:
-// [I, A, B, H, C, F, E, D]
-// [I, A, B, H, C, F, G]
-// [I, T, J, R, O, L, K]
-// [I, T, J, R, O, L, M, N]
-// [I, T, J, R, O, P, Q]
-// [I, T, J, R, S]
+
+
 
 
 
